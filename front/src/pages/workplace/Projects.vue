7d9702d7dd7383118a4e34f1b628d01b65eb78ec
9umpkin <2368322634@qq.com>
2024-05-26 17:57:08 +0800

    111

--- front/src/pages/workplace/Projects.vue
+++ front/src/pages/workplace/Projects.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="projects card ">
+  <div class="projects card">
     <div class="flex items-baseline justify-between w-full">
       <overview-title title="运行进程" subtitle="基本信息" />
     </div>
     <div>
       <a-table :columns="columns" :dataSource="dataSource.value" :pagination="false">
-        <div class="" v-for="items in dataSource" :key="items">
-          <h3>{{ items }}</h3>
-          <div class="flex items-center">
-            <span class="ml-sm">{{ items }}</span>
+        <template #bodyCell="{ text, record, column }">
+          <div v-if="column.dataIndex === 'progress'">
+            <div class="flex items-center">
+              <span class="ml-sm">{{ record[column.dataIndex] }}</span>
+              <a-radio-group>
+                <a-radio-button>终止</a-radio-button>
+              </a-radio-group>
+            </div>
           </div>
-          <a-radio-group>
-            <a-radio-button>终止</a-radio-button>
-          </a-radio-group>
-        </div>
+          <div v-else>
+            {{ text }}
+          </div>
+        </template>
       </a-table>
       <a-button size="large" class="add-btn" type="dashed">
         <template #icon>
@@ -23,23 +27,27 @@
     </div>
   </div>
 </template>
+
 <script lang="ts" setup>
 import OverviewTitle from '@/components/statistic/OverviewTitle.vue';
-import { formatThousand } from '@/utils/formatter';
-import {computed, onMounted, reactive, ref} from "vue";
-import axios from "axios";
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
 const columns = [
   { title: '进程ID', dataIndex: 'pID' },
   { title: '相关操作', dataIndex: 'progress' },
 ];
+
 const dataSource = ref([]);
 
 const getAllTaskInfo = async () => {
   try {
     const response = await axios.get('/all_task_ids');
-    dataSource.value = response.data;
-    console.log('查询成功:', response.data['task_ids']);
+    dataSource.value = response.data.task_ids.map((id: string) => ({
+      pID: id,
+      progress: '运行中'  // 这里可以根据需要修改
+    }));
+    console.log('查询成功:', dataSource.value);
   } catch (error) {
     console.error('Failed to get ids', error);
   }
@@ -48,50 +56,49 @@ const getAllTaskInfo = async () => {
 onMounted(() => {
   getAllTaskInfo();
 });
-
-
 </script>
-<style lang="less" scoped>
-  .projects {
-    :deep(.ant-table) {
-      @apply -mx-md;
 
-      .ant-table-thead {
-        > tr > th:first-child {
+<style lang="less" scoped>
+.projects {
+  :deep(.ant-table) {
+    @apply -mx-md;
+
+    .ant-table-thead {
+      > tr > th:first-child {
+        @apply pl-lg;
+      }
+
+      > tr > th:last-child {
+        @apply pr-lg;
+      }
+    }
+
+    .ant-table-tbody {
+      > tr > td {
+        @apply border-b-0 border-t;
+
+        &:first-child {
           @apply pl-lg;
         }
 
-        > tr > th:last-child {
+        &:last-child {
           @apply pr-lg;
         }
       }
-
-      .ant-table-tbody {
-        > tr > td {
-          @apply border-b-0 border-t;
-
-          &:first-child {
-            @apply pl-lg;
-          }
-
-          &:last-child {
-            @apply pr-lg;
-          }
-        }
-      }
-    }
-
-    :deep(.ant-table-thead > tr > th) {
-      @apply border-none bg-transparent text-subtext;
-
-      &:not(:last-child):not(.ant-table-selection-column):not(.ant-table-row-expand-icon-cell):not([colspan])::before {
-        @apply bg-transparent;
-      }
-    }
-
-    .add-btn {
-      @apply justify-center flex items-center mt-md -mx-2;
-      width: calc(100% + 16px);
     }
   }
+
+  :deep(.ant-table-thead > tr > th) {
+    @apply border-none bg-transparent text-subtext;
+
+    &:not(:last-child):not(.ant-table-selection-column):not(.ant-table-row-expand-icon-cell):not([colspan])::before {
+      @apply bg-transparent;
+    }
+  }
+
+  .add-btn {
+    @apply justify-center flex items-center mt-md -mx-2;
+    width: calc(100% + 16px);
+  }
+}
 </style>
