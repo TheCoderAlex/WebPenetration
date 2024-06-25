3f3d39b7f699c9049deba76f72aef14d3401153e
9umpkin <2368322634@qq.com>
2024-06-25 17:08:43 +0800

    projects list改好啦

--- front/src/pages/workplace/Projects.vue
+++ front/src/pages/workplace/Projects.vue
@@ -6,26 +6,30 @@
     <div>
       <a-table :columns="columns" :dataSource="dataSource" :pagination="false">
         <template #bodyCell="{ text, record, column }">
-          <div v-if="column.dataIndex === 'progress'">
+          <!-- 显示所有记录的 status 列 -->
+          <div v-if="column.dataIndex === 'status'">
+            {{ record[column.dataIndex] }}
+          </div>
+
+          <!-- 自定义 progress 列 -->
+          <div v-else-if="column.dataIndex === 'progress'">
             <div class="flex items-center">
-              <span class="ml-sm p-2">{{ record[column.dataIndex] }}</span>
-                <a-radio-group>
-                  <a-radio-button @click="terminateTask(record.pID)">终止</a-radio-button>
-                </a-radio-group>
+              <!-- 显示 progress 的值 -->
+<!--              <span class="ml-sm p-2">{{ record[column.dataIndex] }}</span>-->
+              <!-- 仅当 progress 存在时显示终止按钮 -->
+              <a-radio-group v-if="record[column.dataIndex]">
+                <a-radio-button @click="terminateTask(record.pID)">终止</a-radio-button>
+              </a-radio-group>
             </div>
           </div>
 
-          <div v-else-if="column.dataIndex === 'result'">
-            <div class="flex items-center">
-              123
-            </div>
-          </div>
-
+          <!-- 其他列显示默认文本内容 -->
           <div v-else>
             {{ text }}
           </div>
         </template>
       </a-table>
+
       <a-button size="large" class="add-btn" type="dashed" @click="goToWorkplace">
         <template #icon>
         </template>
@@ -44,7 +48,7 @@ import router from "@/router";
 
 const columns = [
   { title: '进程ID', dataIndex: 'pID' },
-  { title: '运行结果', dataIndex: 'result'},
+  { title: '运行状态', dataIndex: 'status'},
   { title: '相关操作', dataIndex: 'progress' },
 ];
 const dataSource = ref([]);
@@ -52,15 +56,60 @@ const dataSource = ref([]);
 const getAllTaskInfo = async () => {
   try {
     const response = await axios.get('/all_task_ids');
-    dataSource.value = response.data.task_ids.map((id: string) => ({
+
+    const newEntries = response.data.task_ids.map((id: string) => ({
       pID: id,
-      progress: '运行中'
+      status: '运行中',
+      progress: 'terminate'
     }));
-    console.log('查询成功:', response.data['task_ids']);
+
+    newEntries.forEach((entry: void) => {
+      dataSource.value.push(entry);
+    })
+    console.log('runningTasks查询成功:', dataSource.value, '=======', response.data.task_ids);
   } catch (error) {
     console.error('Failed to get ids', error);
   }
 };
+const addTaskIdsDataSource = (response, status): void => {
+  const newEntries = response.data.task_ids.map((id: string) => ({
+      pID: id,
+      status: status
+    }));
+  newEntries.forEach((entry: void) => {
+    dataSource.value.push(entry);
+  })
+}
+const getSuccessTaskInfo = async () => {
+  try {
+    const response = await axios.get('/success_task_ids');
+    addTaskIdsDataSource(response, '运行成功');
+    console.log('successTasks查询成功:', dataSource.value, '=======', response.data.task_ids);
+  } catch (error) {
+    console.error('Failed to get ids', error);
+  }
+};
+
+const getFailedTaskInfo = async () => {
+  try {
+    const response = await axios.get('/failed_task_ids');
+    addTaskIdsDataSource(response, '运行失败');
+    console.log('failedTasks查询成功:', dataSource.value, '=======', response.data.task_ids);
+  } catch (error) {
+    console.error('Failed to get ids', error);
+  }
+};
+
+const getTerminatedTaskInfo = async () => {
+  try {
+    const response = await axios.get('/terminated_task_ids');
+    addTaskIdsDataSource(response, '已终止');
+    console.log('terminatedTasks查询成功:', dataSource.value, '=======', response.data.task_ids);
+  } catch (error) {
+    console.error('Failed to get ids', error);
+  }
+};
+
 const terminateTask = async (taskId: string) => {
   try{
     const response = await axios.post('/terminate_task', {task_id: taskId});
@@ -76,6 +125,9 @@ const goToWorkplace = () =>{
 
 onMounted(() => {
   getAllTaskInfo();
+  getTerminatedTaskInfo();
+  getSuccessTaskInfo();
+  getFailedTaskInfo();
 });
 </script>
 
